86815eba2eb5ce3e7903010d8cc8364a1fb4baad
Simon Xu <simonxu@Simons-MacBook-Pro.local>
2018-12-28 09:55:08 -0800

    added REST post to database

--- Firebasetest/functions/index.js
+++ Firebasetest/functions/index.js
@@ -1,21 +1,52 @@
+const admin = require('firebase-admin');
+
 const functions = require('firebase-functions');
 const express = require('express');
 
 const app = express();
+// app.use(express.urlencoded({ extended: true }));
 
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`);
+// Initialize the database
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
+
+// ------ End test
+
+app.get('/database', (req, res) => {
+  res.send('get received');
 });
 
-// Cached version
-app.get('/timestamp-cached', (request, response) => {
-  response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-  response.send(`${Date.now()}`);
-});
+// // Cached version
+// app.get('/timestamp-cached', (request, response) => {
+//   response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+//   response.send(`${Date.now()}`);
+// });
 
-app.post('/timestamp', (req, res) => {
+app.post('/database', (req, res) => {
   res.send('POST sent');
+  console.log(req.headers['content-type']);
+  console.log(req.query);
+  console.log(req.body['key1']);   // Shows the value of the key
+  console.dir(req.body);                  // console.dir shows data inside the object
+
+  // var Device1Ref = db.collection('devices').doc('Device1');
+  // var setDevice = Device1Ref.set({
+  //   'key1': req.body['key1'],
+  //   'key2': req.body['key2']
+  // })
+
+  // var aTuringRef = db.collection('devices').doc('aturing');
+  // var setAlan = aTuringRef.set({
+  //   'first': 'Alan',
+  //   'middle': 'Mathison',
+  //   'last': 'Turing',
+  //   'born': 1912
+  // });
+
 });
+
+
 
 exports.app = functions.https.onRequest(app);
+
 
--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const routes = require('./routes/api.js');
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 // To save lines, can also do 
 // app.use('/api, require('./routes/api.js')) 
 
 app.listen(process.env.port || 4000, function(){
-  console.log('now listening for requests');
+  console.log('now listening for requests on localhost:4000');
 });
